e0a3481631339baa51f206a73c4d753d0008b1b9
abidta <sainulabidofficial@gmail.com>
2025-09-27 19:13:51 +0000

    feat: add daisy ui
    add button component, change primary color to design color
    
    add prettier in frontend

---
+++ frontend/src/app/components/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { Button } from '@/components/Button';
+import React from 'react';
+
+function page() {
+  return (
+    <div className="flex ml-5 mt-5">
+      <Button> Test Button</Button>
+    </div>
+  );
+}
+
+export default page;
